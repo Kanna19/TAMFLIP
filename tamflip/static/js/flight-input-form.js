fe3c28594dd4f2582ec880221717a42b7510f703
RamAnudeep99 <ramanudeep.1999@gmail.com>
2020-05-01 18:00:02 +0530

    Adding html, css, js files

---
+++ tamflip/static/js/flight-input-form.js
@@ -0,0 +1,33 @@
+
+$(".type21").on("click", function(){
+  $("#type2").text("Economy");
+});
+
+$(".type22").on("click", function(){
+  $("#type2").text("Premium Economy");
+});
+
+$(".type23").on("click", function(){
+  $("#type2").text("Business");
+});
+
+$(".type24").on("click", function(){
+  $("#type2").text("First Class");
+});
+
+$(".passenger").on("click", function(){
+  var adlt = $(".adlt").val();
+  var chld = $(".chld").val();
+  var inft = $(".inft").val();
+  var sum = parseInt(adlt, 10) + parseInt(chld, 10) + parseInt(inft, 10);
+
+  $("#type1Num").text(sum);
+
+  if(sum <= 1){
+    $("#type1Txt").text("Passenger");
+  }
+
+  if(sum > 1){
+    $("#type1Txt").text("Passengers");
+  }
+});
