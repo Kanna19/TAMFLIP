c2f6a9553967251ca8a71e42be7b87f12b3492c4
RamAnudeep99 <ramanudeep.1999@gmail.com>
2020-05-04 16:57:04 +0530

    Added Type of Class to Form

--- tamflip/static/js/flight-input-form.js
+++ tamflip/static/js/flight-input-form.js
@@ -1,22 +1,26 @@
 // $("body").on("click", function(){
-//   $(this).css("background", "green");
+  // $(this).css("background", "green");
 // })
 
 
 $(".type21").on("click", function(){
   $("#type2").text("Economy");
+  $(".hiddenInput").val("Economy");
 });
 
 $(".type22").on("click", function(){
   $("#type2").text("Premium Economy");
+  $(".hiddenInput").val("Premium Economy");
 });
 
 $(".type23").on("click", function(){
   $("#type2").text("Business");
+  $(".hiddenInput").val("Business");
 });
 
 $(".type24").on("click", function(){
   $("#type2").text("First Class");
+  $(".hiddenInput").val("First Class");
 });
 
 $(".passenger").on("click", function(){
