7a38a2d900c1644b606f8a89b2a80699270395ad
Kanna19 <kannasasuke19@gmail.com>
2020-05-05 00:58:02 +0530

    Added a cron job which gets triggered at 9am

--- tamflip/__init__.py
+++ tamflip/__init__.py
@@ -1,9 +1,11 @@
-#Application Factory
+# Application Factory
 import os
 from flask import Flask
 from flask import render_template
 from flask import request, g
+
 from . import apiModule
+from . import flight_tracker
 
 from apscheduler.jobstores.memory import MemoryJobStore
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -11,38 +13,8 @@ from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
 from pytz import utc
 
 
-class Config(object):
-    JOBS = [
-        {
-            'id': 'job1',
-            'func': '__init__:job1',
-            'args': (1, 2),
-            'trigger': 'interval',
-            'seconds': 5
-        }
-    ]
-
-    SCHEDULER_JOBSTORES = {
-        'default': MemoryJobStore()
-    }
-
-    SCHEDULER_EXECUTORS = {
-        'default': {'type': 'threadpool', 'max_workers': 20}
-    }
-
-    SCHEDULER_JOB_DEFAULTS = {
-        'coalesce': False,
-        'max_instances': 3
-    }
-
-    SCHEDULER_API_ENABLED = True
-
-
-def job1():
-    print("DID JOB!!")
-
 def create_app(test_config=None):
-    #create and configure the app
+    # Create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
@@ -50,7 +22,7 @@ def create_app(test_config=None):
 
     )
 
-    #ensure instance folder exists
+    # Ensure instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
@@ -66,17 +38,29 @@ def create_app(test_config=None):
         'max_instances': 3
     }
 
-    #Link scheduler
+    # Link scheduler
     if os.environ.get("WERKZEUG_RUN_MAIN") != "true":
-        scheduler = BackgroundScheduler(jobstores=jobstores, executors=executors, job_defaults=job_defaults, timezone=utc)
-        scheduler.add_job(job1, 'interval', seconds=5)
+        scheduler = BackgroundScheduler(
+            jobstores=jobstores,
+            executors=executors,
+            job_defaults=job_defaults,
+            timezone=utc
+        )
+        # Add a cron job which gets triggered every day at 0330 UTC.
+        scheduler.add_job(
+            flight_tracker.send_alerts_to_subscribed_users,
+            trigger='cron',
+            args=[app],
+            hour='3',
+            minute='30'
+        )
         scheduler.start()
 
     # Link with Database
     from . import db
     db.init_app(app)
 
-    #Link blueprints
+    # Link blueprints
     from . import index
     app.register_blueprint(index.bp)
 
