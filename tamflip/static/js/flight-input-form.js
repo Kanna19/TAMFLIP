2dba47b31bedc56abcbac3d9be83702624aadbf3
RamAnudeep99 <ramanudeep.1999@gmail.com>
2020-05-07 16:42:52 +0530

    Added Sort functionality

--- tamflip/static/js/flight-input-form.js
+++ tamflip/static/js/flight-input-form.js
@@ -61,25 +61,7 @@ $( "button[id*='track']" ).on("click", function(){
     $(input_id).toggleClass("hidden-input");
 });
 
-// var $divs = $(".flight-box");
 
-// $(".type42").on("click", function () {
-//     var count = 0;
-//     var alphabeticallyOrderedDivs = $divs.sort(function (a, b) {
-//         return $(a).(".fbox1 .fbox3").text() > $(b).(".fbox1 .fbox3").text();
-//         count = count + 1;
-//     });
-//     $("#track-details").html(alphabeticallyOrderedDivs);
-//     alert{count};
-// });
-//
-// $(".type43").on("click", function () {
-//     alert("clicked");
-//     var alphabeticallyOrderedDivs = $divs.sort(function (a, b) {
-//         return $(a).find("#departTime").text() > $(b).find(".departTime").text();
-//     });
-//     $("#track-details").html(alphabeticallyOrderedDivs);
-// });
 var loadData = 10;
 $(".moreButton").on("click",function(){
      for(var i = 1; i<=10; i++)
@@ -95,3 +77,47 @@ $(".moreButton").on("click",function(){
      loadData = loadData+10;
 });
 
+// var $divs = $(".flight-box");
+
+// $(".type42").on("click", function () {
+//     var count = 0;
+//     var alphabeticallyOrderedDivs = $divs.sort(function (a, b) {
+//         return $(a).(".fbox1 .fbox3").text() > $(b).(".fbox1 .fbox3").text();
+//         count = count + 1;
+//     });
+//     $("#track-details").html(alphabeticallyOrderedDivs);
+//     alert{count};
+// });
+
+$(".type41").on("click", function(){
+  divArr = $(".flight-box")
+  divArr.sort(function(a, b) {
+          return $(a).find(".price").text() > $(b).find(".price").text() ? 1: -1;
+      })
+  $("#track-details").append(divArr)
+});
+
+
+$(".type42").on("click", function(){
+  divArr = $(".flight-box")
+  divArr.sort(function(a, b) {
+          return $(a).find(".fbox3").text() > $(b).find(".fbox3").text() ? 1: -1;
+      })
+  $("#track-details").append(divArr)
+});
+
+$(".type43").on("click", function(){
+  divArr = $(".flight-box")
+  divArr.sort(function(a, b) {
+          return $(a).find(".departTime").text() > $(b).find(".departTime").text() ? 1: -1;
+      })
+  $("#track-details").append(divArr)
+});
+
+$(".type44").on("click", function(){
+  divArr = $(".flight-box")
+  divArr.sort(function(a, b) {
+          return $(a).find(".arrivalTime").text() > $(b).find(".arrivalTime").text() ? 1: -1;
+      })
+  $("#track-details").append(divArr)
+});
